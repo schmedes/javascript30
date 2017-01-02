8dcc276b91225de53ea0041652476d89f215e5ed
schmedes <julian.schmedes@gmx.de>
2017-01-02 13:15:44 +0100

    add basic functionality for clock hands

--- 02-CSSJsClock/index.js
+++ 02-CSSJsClock/index.js
@@ -3,8 +3,11 @@ CSS + JS Clock
 - get current time with javascript
 - update the hands accordingly
 */
-// reference to digital clock dom element
+// reference to dom elements
 const digitalClock = document.querySelector('.digital');
+const hoursHand = document.querySelector('.hour');
+const minutesHand = document.querySelector('.minute');
+const secondsHand = document.querySelector('.second');
 
 // function to extract the exact values
 const getTime = date => ({
@@ -15,6 +18,9 @@ const getTime = date => ({
 
 const timeTick = _ => {
     const {hours, minutes, seconds} = getTime(new Date());
+    secondsHand.style.transform = `rotate(${(seconds * 6) - 270}deg)`;
+    minutesHand.style.transform = `rotate(${(minutes * 6) - 270}deg)`;
+    hoursHand.style.transform = `rotate(${(hours * 30) - 270}deg)`;
     digitalClock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
