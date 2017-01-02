75db341b99636b39a5ca796e0058f2b64547859c
schmedes <julian.schmedes@gmx.de>
2017-01-02 12:43:04 +0100

    add basic functionality

--- 02-CSSJsClock/index.js
+++ 02-CSSJsClock/index.js
@@ -3,3 +3,20 @@ CSS + JS Clock
 - get current time with javascript
 - update the hands accordingly
 */
+// reference to digital clock dom element
+const digitalClock = document.querySelector('.digital');
+
+// function to extract the exact values
+const getTime = date => ({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+})
+
+const timeTick = _ => {
+    const {hours, minutes, seconds} = getTime(new Date());
+    digitalClock.innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(timeTick, 1000);
+
