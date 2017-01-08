504313a08bf232cc6da7b78a73e1cbe4db12da87
schmedes <julian.schmedes@gmx.de>
2017-01-08 19:46:06 +0100

    finish day8

--- 08-HTMLCanvas/index.js
+++ 08-HTMLCanvas/index.js
@@ -3,3 +3,29 @@ const context = canvas.getContext('2d');
 context.width = window.innerWidth;
 context.height = window.innerHeight;
 
+context.lineJoin = 'round';
+context.lineCap = 'round';
+context.lineWidth = 5;
+context.strokeStyle = '#121a4e';
+
+let drawing = false;
+let x = 0;
+let y = 0;
+
+const draw = event =>{
+    if(!drawing) return;
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(event.offsetX, event.offsetY);
+    context.stroke();
+    [x, y] = [event.offsetX, event.offsetY];
+}
+
+canvas.addEventListener('mousedown', (e)=>{
+    drawing = true;
+    [x, y] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', ()=> drawing = false);
+canvas.addEventListener('mouseout', ()=> drawing = false);
